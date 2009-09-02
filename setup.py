44250110da01fba60c8b70e5a27866e82228993c
Jannis Leidel <jannis@leidel.info>
2009-09-02 16:52:49 +0200

    Bumped to 0.1.2dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name='django-staticfiles',
-    version='0.1.1',
+    version='0.1.2',
     description="A Django app that provides helpers for serving static files.",
     long_description=read('README'),
     author='Jannis Leidel',
