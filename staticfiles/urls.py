0684e716620199d813bc02537660a285adb14478
Chris Beaven <smileychris@gmail.com>
2009-11-02 12:22:05 +1300

    Small changes to URLs to only serve MEDIA_ROOT files if both MEDIA_ROOT and MEDIA_URL settings are provided.

--- staticfiles/urls.py
+++ staticfiles/urls.py
@@ -10,7 +10,8 @@ if ':' not in settings.STATIC_URL:
         url(r'^%s(?P<path>.*)$' % base_url, 'serve'),
     )
 
-if ':' not in settings.MEDIA_URL:
+if settings.MEDIA_ROOT and settings.MEDIA_URL and \
+                        ':' not in settings.MEDIA_URL:
     base_url = re.escape(settings.MEDIA_URL[1:])
     urlpatterns += patterns('django.views.static',
         url(r'^%s(?P<path>.*)$' % base_url, 'serve',
