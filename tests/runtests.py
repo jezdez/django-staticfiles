67a8acf91a7de2afca55c9a38a4ea6e100839f0e
Jannis Leidel <jannis@leidel.info>
2010-02-25 02:19:10 +0100

    Made testrunner compatible with Django 1.1.X and 1.2.X.

--- tests/runtests.py
+++ tests/runtests.py
@@ -7,12 +7,11 @@ sys.path.insert(0, parent)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 
-from django.test.utils import get_runner
+from django.test.simple import run_tests
 from django.conf import settings
 
 def runtests():
-    test_runner = get_runner(settings)
-    failures = test_runner(['tests'], verbosity=1, interactive=True)
+    failures = run_tests(['tests'], verbosity=1, interactive=True)
     sys.exit(failures)
 
 if __name__ == '__main__':
