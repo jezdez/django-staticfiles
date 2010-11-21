7f7ca5487db526b1e139448796f0d33f5561ddf1
Jannis Leidel <jannis@leidel.info>
2010-11-21 17:34:13 +0100

    YEAH

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+print "yeah"
+
 setup(
     name='django-staticfiles',
     version='0.3.2',
