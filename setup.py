e98a0f7575a67b4e8d5c9120536ce218ce1b4575
Jannis Leidel <jannis@leidel.info>
2010-12-25 13:03:56 +0100

    Removed embarrassing print statement.

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-
-print "yeah"
-
 setup(
     name='django-staticfiles',
     version='0.3.3',
