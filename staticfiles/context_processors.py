bb6af894cf200a91dac882a93dfea9a6477d2b11
Jannis Leidel <jannis@leidel.info>
2011-01-27 15:30:55 +0100

    Added backwards compatible context processor.

--- staticfiles/context_processors.py
+++ staticfiles/context_processors.py
@@ -1,9 +1,15 @@
+import warnings
 from django.conf import settings
 
-def static_url(request):
+def static(request):
     """
     Adds static-related context variables to the context.
-
     """
     return {'STATIC_URL': settings.STATIC_URL}
 
+def static_url(request):
+    warnings.warn(
+        "The context processor 'staticfiles.context_processors.static_url' "
+        "was renamed to 'staticfiles.context_processors.static'.",
+        PendingDeprecationWarning)
+    return static(request)
